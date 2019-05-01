59cbd0fbaf2d62d9db2788348ec432baf77976a7
ronald <r.gaskin@live.ca>
2019-05-01 15:30:44 -0700

    added-script-and-jquerry-need-to-build-proper-box

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 import { lookup } from "dns";
 
-function sayHello(){
+function sayHello() {
     console.log('hello world');
 }
 sayHello();
 
 $('#login').click(() => {
     $('#loginMenu').toggle()
-  });
+});
 
 
 
 $.ajax({
-
+    method: 'get',
+    url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=xtixYn4X7hTAoUFaf0hdsTQrtrEKkqYS'
 })
+    .done(function (data) {
+        console.log(data);
+    });
